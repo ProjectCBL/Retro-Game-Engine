555c2177c7347ae8a1845aae78c398ee534b61ce
ProjectCBL <projectxcbl@gmail.com>
2022-04-20 09:22:53 -0400

    Linked engine with game class with a has-a relationship

--- core/src/com/retro/engine/Game.java
+++ core/src/com/retro/engine/Game.java
@@ -1,31 +1,28 @@
 package com.retro.engine;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	Engine engine = Engine.getInstance();
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		engine.setUp();
 	}
 
 	@Override
 	public void render () {
+		float dt = Gdx.graphics.getDeltaTime();
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		engine.run(dt);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		engine.dispose();
 	}
 }
