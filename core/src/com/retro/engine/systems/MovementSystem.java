7ac64802e77b1d27372385d5352793427d7d5f98
ProjectCBL <projectxcbl@gmail.com>
2022-04-24 15:22:25 -0400

    Created a basic movement system class

--- core/src/com/retro/engine/systems/MovementSystem.java
+++ core/src/com/retro/engine/systems/MovementSystem.java
@@ -1,2 +1,20 @@
-package com.retro.engine.systems;public class MovementSystem {
+package com.retro.engine.systems;
+
+public class MovementSystem implements System{
+
+    @Override
+    public void setUp() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
 }
