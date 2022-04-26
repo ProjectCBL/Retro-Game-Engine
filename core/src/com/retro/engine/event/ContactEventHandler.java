e9ce01cc2edd273574b9e35f234ebf9b250299fd
ProjectCBL <projectxcbl@gmail.com>
2022-04-25 20:35:17 -0400

    Implemented Contact Listener interface

--- core/src/com/retro/engine/event/ContactEventHandler.java
+++ core/src/com/retro/engine/event/ContactEventHandler.java
@@ -1,2 +1,30 @@
-package com.retro.engine.event;public class ContactHandler {
+package com.retro.engine.event;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class ContactEventHandler implements ContactListener {
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+
 }
