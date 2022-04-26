746e34e009ea4c3ba8af073fc98b0bb467cb15ae
ProjectCBL <projectxcbl@gmail.com>
2022-04-25 20:32:31 -0400

    Added a body and fixture attribute for box2d collider specific objects

--- core/src/com/retro/engine/components/ColliderComponent.java
+++ core/src/com/retro/engine/components/ColliderComponent.java
@@ -1,2 +1,9 @@
-package com.retro.engine.components;public class ColliderComponent {
+package com.retro.engine.components;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+
+public class ColliderComponent {
+    Body body;
+    Fixture fixture;
 }
