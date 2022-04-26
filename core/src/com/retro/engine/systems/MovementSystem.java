80b8167f518595117458b890a4f5835df74969d6
ProjectCBL <projectxcbl@gmail.com>
2022-04-25 20:37:41 -0400

    Converted class to abstract class

--- core/src/com/retro/engine/systems/MovementSystem.java
+++ core/src/com/retro/engine/systems/MovementSystem.java
@@ -1,20 +1,5 @@
 package com.retro.engine.systems;
 
-public class MovementSystem implements System{
-
-    @Override
-    public void setUp() {
-
-    }
-
-    @Override
-    public void update() {
-
-    }
-
-    @Override
-    public void dispose() {
-
-    }
+public abstract class MovementSystem implements System{
 
 }
